3a101dd37d43c29d44a4c87594857a8a93ee7c20
Onsentamagoyaki <souma.20030719@gmail.com>
2024-10-11 01:16:51 +0900

    commitTest

---
+++ Assets/Script/ExplodeEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class ExplodeEffect : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        Instantiate(_particle,transform.position,_particle.transform.rotation);
+    }
+}
